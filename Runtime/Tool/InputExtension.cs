0573bc406e103be120ec50e01bfb9aa539239aed
Strix <KorStrix@users.noreply.github.com>
2020-01-20 15:36:25 +0900

    no message

---
+++ Runtime/Tool/InputExtension.cs
@@ -0,0 +1,40 @@
+﻿#region Header
+/*	============================================
+ *	작성자 : Strix
+ *	작성일 : 2020-01-20 오후 2:29:41
+ *	개요 : 
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public static class InputExtension
+{
+    static public float GetZoomInOut_ChangedDelta()
+    {
+#if UNITY_EDITOR
+        return -Input.mouseScrollDelta.y;
+ #elif UNITY_ANDROID || UNITY_IOS
+        if(Input.touchCount >= 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            return prevTouchDeltaMag - touchDeltaMag;
+        }
+#endif
+
+        return 0f;
+    }
+}
---
+++ Runtime/Tool/InputExtension_Tester_ZoomInOut.cs
@@ -0,0 +1,60 @@
+﻿#region Header
+/*	============================================
+ *	작성자 : Strix
+ *	작성일 : 2020-01-20 오후 2:30:29
+ *	개요 : 
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class InputExtension_Tester_ZoomInOut : MonoBehaviour
+{
+    /* const & readonly declaration             */
+
+    /* enum & struct declaration                */
+
+    /* public - Field declaration            */
+
+    public float FOV_Max = 100f;
+    public float FOV_Min = 20f;
+    public float fZoomSpeed = 5f;
+
+    /* protected & private - Field declaration         */
+
+
+    // ========================================================================== //
+
+    /* public - [Do] Function
+     * 외부 객체가 호출(For External class call)*/
+
+
+    // ========================================================================== //
+
+    /* protected - Override & Unity API         */
+
+    private void Update()
+    {
+        float fChangedDelta = InputExtension.GetZoomInOut_ChangedDelta();
+        if(fChangedDelta != 0f)
+        {
+            Debug.Log(fChangedDelta);
+            Camera.main.fieldOfView += fChangedDelta * fZoomSpeed;
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, FOV_Min, FOV_Max);
+        }
+    }
+
+    /* protected - [abstract & virtual]         */
+
+
+    // ========================================================================== //
+
+    #region Private
+
+    #endregion Private
+}
