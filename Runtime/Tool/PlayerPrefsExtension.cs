8c628ea41089c70ef0fe272a5a4ee6fd97c3397f
Strix <KorStrix@users.noreply.github.com>
2020-01-21 18:52:10 +0900

    no message

---
+++ Runtime/Tool/PlayerPrefsExtension.cs
@@ -0,0 +1,203 @@
+﻿#region Header
+/*	============================================
+ *	작성자 : Strix
+ *	작성일 : 2020-01-21 오후 4:36:26
+ *	개요 : 
+ *	시리얼라이징 가능한 오브젝트를 저장하는 기능
+ *	암호화 기능
+ *	
+ *	암호화 참고 링크 : https://gist.github.com/ftvs/5299600
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Unity_Pattern
+{
+    /// <summary>
+    /// <see cref="UnityEngine.PlayerPrefs"/>에서 기능을 추가한 클래스입니다.
+    /// </summary>
+    public static class PlayerPrefsExtension
+    {
+        #region 암호화 관련
+
+        const string g_strPrivateKey = "암호화해야지";
+        const string const_strEncryption_Check = "_encryption_check";
+        const string const_strUsedKey = "_used_key";
+
+        static readonly System.Random g_pRandom = new System.Random(g_strPrivateKey.GetHashCode());
+        static readonly UTF8Encoding g_Encoding = new UTF8Encoding();
+        static readonly MD5CryptoServiceProvider g_MD5Provider = new MD5CryptoServiceProvider();
+        static readonly TripleDESCryptoServiceProvider g_TripleDescryptoProvider = Generate_TripleDescryptoProvider();
+
+        private static TripleDESCryptoServiceProvider Generate_TripleDescryptoProvider()
+        {
+            TripleDESCryptoServiceProvider pProvider = new TripleDESCryptoServiceProvider();
+            pProvider.Mode = CipherMode.ECB;
+            pProvider.Padding = PaddingMode.PKCS7;
+
+            return pProvider;
+        }
+
+        public class EncryptWrapper
+        {
+            public string strKey;
+            public string strData;
+
+            public EncryptWrapper(string strKey, string strData)
+            {
+                this.strKey = strKey; this.strData = strData;
+            }
+        }
+
+        public static string[] g_arrKeys = new string[] { "디폴트키", "입니다", "변경을 권장합니다" };
+
+        public static byte[] Get_MD5Hash(string strToEncrypt)
+        {
+            return g_MD5Provider.ComputeHash(g_Encoding.GetBytes(strToEncrypt));
+        }
+
+        public static void SaveEncryption(string strKey, string strType, string strValue)
+        {
+            int iKeyIndex_Random = g_pRandom.Next(int.MinValue, int.MaxValue) % g_arrKeys.Length;
+            string strSecretKey = g_arrKeys[iKeyIndex_Random];
+            byte[] arrKey = GetCheckSum(strType, strSecretKey);
+            byte[] arrData = g_Encoding.GetBytes(strValue);
+
+            g_TripleDescryptoProvider.Key = arrKey;
+            ICryptoTransform transform = g_TripleDescryptoProvider.CreateEncryptor();
+            byte[] arrEncryptedData = transform.TransformFinalBlock(arrData, 0, arrData.Length);
+
+            PlayerPrefs.SetString(strKey, System.Convert.ToBase64String(arrEncryptedData));
+            PlayerPrefs.SetInt(strKey + const_strUsedKey, iKeyIndex_Random);
+        }
+
+        public static bool GetEncryptedData(string strKey, string strType, out string strValue)
+        {
+            strValue = "";
+            if (PlayerPrefs.HasKey(strKey) == false || PlayerPrefs.HasKey(strKey + const_strUsedKey) == false)
+                return false;
+
+            int iKeyIndex_Random = PlayerPrefs.GetInt(strKey + const_strUsedKey);
+            string strSecretKey = g_arrKeys[iKeyIndex_Random];
+            byte[] arrKey = GetCheckSum(strType, strSecretKey);
+
+            string strEncryptedData = PlayerPrefs.GetString(strKey);
+            byte[] arrEncryptedData = System.Convert.FromBase64String(strEncryptedData);
+
+            g_TripleDescryptoProvider.Key = arrKey;
+            ICryptoTransform transform = g_TripleDescryptoProvider.CreateDecryptor();
+            byte[] results = transform.TransformFinalBlock(arrEncryptedData, 0, arrEncryptedData.Length);
+            strValue = g_Encoding.GetString(results);
+
+            return true;
+        }
+
+        public static void DeleteKey(string strKey)
+        {
+            PlayerPrefs.DeleteKey(strKey);
+            PlayerPrefs.DeleteKey(strKey + const_strEncryption_Check);
+            PlayerPrefs.DeleteKey(strKey + const_strUsedKey);
+        }
+
+
+        private static byte[] GetCheckSum(string strType, string strSecretKey)
+        {
+            return Get_MD5Hash(strType + "_" + g_strPrivateKey + "_" + strSecretKey);
+        }
+
+
+        #endregion 암호화 관련
+
+        static public void SetObject(string strKey, object pSerializeObject, bool bPrint_OnError = true)
+        {
+            string strJson = JsonUtility.ToJson(pSerializeObject);
+            if (Check_IsInvalidJson(strJson))
+            {
+                if (bPrint_OnError)
+                    Debug.LogError($"{nameof(SetObject)} - ToJson() Fail - {strKey}-{pSerializeObject.ToString()}");
+
+                return;
+            }
+
+            PlayerPrefs.SetString(strKey, strJson);
+        }
+
+        static public void SetObject_Encrypt(string strKey, object pSerializeObject, bool bPrint_OnError = true)
+        {
+            string strJson = JsonUtility.ToJson(pSerializeObject);
+            if (Check_IsInvalidJson(strJson))
+            {
+                if (bPrint_OnError)
+                    Debug.LogError($"{nameof(SetObject)} - ToJson() Fail - {strKey}-{pSerializeObject.ToString()}");
+
+                return;
+            }
+
+            SetObject(strKey, pSerializeObject, bPrint_OnError);
+            SaveEncryption(strKey, pSerializeObject.GetType().Name, strJson);
+        }
+
+        static public bool GetObject<T>(string strKey, ref T pGetObject, bool bPrint_OnError = true)
+        {
+            if (PlayerPrefs.HasKey(strKey) == false)
+            {
+                if (bPrint_OnError)
+                    Debug.LogError($"{nameof(GetObject)} - PlayerPrefs.HasKey({strKey}) == false");
+
+                return false;
+            }
+
+            string strJson = PlayerPrefs.GetString(strKey);
+            if (Check_IsInvalidJson(strJson))
+            {
+                if (bPrint_OnError)
+                    Debug.LogError($"{nameof(GetObject)} - ToJson() Fail - {strKey}");
+
+                return false;
+            }
+
+            try
+            {
+                JsonUtility.FromJsonOverwrite(strJson, pGetObject);
+            }
+            catch (System.Exception e)
+            {
+                if (bPrint_OnError)
+                    Debug.LogError($"{nameof(GetObject)} - FromJsonOverwrite Fail - {strKey} Value : \n{strJson}");
+
+                return false;
+            }
+
+            return true;
+        }
+
+        static public bool GetObject_Encrypted<T>(string strKey, ref T pGetObject, bool bPrint_OnError = true)
+        {
+            string strJson;
+            if (GetEncryptedData(strKey, pGetObject.GetType().Name, out strJson) == false)
+            {
+                if (bPrint_OnError)
+                    Debug.LogError($"{nameof(GetObject_Encrypted)} - Check Encrypt() Fail - {strKey}");
+
+                return false;
+            }
+            JsonUtility.FromJsonOverwrite(strJson, pGetObject);
+
+            return true;
+        }
+
+
+        private static bool Check_IsInvalidJson(string strJson)
+        {
+            if (string.IsNullOrEmpty(strJson) || strJson.Equals("{}") == false)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Tests/Runtime/PlayerPrefsExtension_Tester.cs
@@ -0,0 +1,117 @@
+﻿#region Header
+/*	============================================
+ *	작성자 : Strix
+ *	작성일 : 2020-01-21 오후 4:43:48
+ *	개요 : 
+   ============================================ */
+#endregion Header
+
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using Unity_Pattern;
+using UnityEngine.TestTools;
+
+namespace StrixLibrary_Test
+{
+    [Category("StrixLibrary")]
+    public class PlayerPrefsWrapper_Tester
+    {
+        [System.Serializable]
+        public class SaveTest_Safe
+        {
+            public int iValue;
+            public string strValue;
+            public float fValue;
+        }
+
+        public class SaveTest_UnSafe
+        {
+            [System.NonSerialized]
+            public int iValue;
+            [System.NonSerialized]
+            public string strValue;
+            [System.NonSerialized]
+            public float fValue;
+        }
+
+        [Test]
+        public void SaveLoad_BasicTest()
+        {
+            SaveTest_Safe pTest = new SaveTest_Safe();
+            pTest.iValue = Random.Range(1, 100);
+            pTest.strValue = pTest.iValue.ToString();
+            pTest.fValue = Random.Range(-1f, 1f);
+
+            PlayerPrefsExtension.SetObject(nameof(SaveLoad_BasicTest), pTest);
+
+            SaveTest_Safe pTest_ForCheck = new SaveTest_Safe();
+
+            Assert.AreNotEqual(pTest.iValue, pTest_ForCheck.iValue);
+            Assert.AreNotEqual(pTest.strValue, pTest_ForCheck.strValue);
+            Assert.AreNotEqual(pTest.fValue, pTest_ForCheck.fValue);
+
+            bool bResult_Safe = PlayerPrefsExtension.GetObject(nameof(SaveLoad_BasicTest), ref pTest_ForCheck);
+            Assert.AreEqual(bResult_Safe, true);
+
+            Assert.AreEqual(pTest.iValue, pTest_ForCheck.iValue);
+            Assert.AreEqual(pTest.strValue, pTest_ForCheck.strValue);
+            Assert.AreEqual(pTest.fValue, pTest_ForCheck.fValue);
+
+            // Error - Not Found Key
+            bResult_Safe = PlayerPrefsExtension.GetObject("Test2", ref pTest_ForCheck, false);
+            Assert.AreEqual(bResult_Safe, false);
+
+            SaveTest_UnSafe pTest_UnSafe = new SaveTest_UnSafe();
+            pTest_UnSafe.iValue = Random.Range(1, 100);
+            pTest_UnSafe.strValue = pTest.iValue.ToString();
+            pTest_UnSafe.fValue = Random.Range(-1f, 1f);
+
+            PlayerPrefsExtension.SetObject("Test_UnSafe", pTest_UnSafe, false);
+
+            SaveTest_UnSafe pTest_ForCheck_UnSafe = new SaveTest_UnSafe();
+
+            Assert.AreNotEqual(pTest_UnSafe.iValue, pTest_ForCheck_UnSafe.iValue);
+            Assert.AreNotEqual(pTest_UnSafe.strValue, pTest_ForCheck_UnSafe.strValue);
+            Assert.AreNotEqual(pTest_UnSafe.fValue, pTest_ForCheck_UnSafe.fValue);
+
+            bool bResult_UnSafe = PlayerPrefsExtension.GetObject("Test_UnSafe", ref pTest_ForCheck_UnSafe, false);
+            Assert.AreEqual(bResult_UnSafe, false);
+
+            Assert.AreNotEqual(pTest_UnSafe.iValue, pTest_ForCheck_UnSafe.iValue);
+            Assert.AreNotEqual(pTest_UnSafe.strValue, pTest_ForCheck_UnSafe.strValue);
+            Assert.AreNotEqual(pTest_UnSafe.fValue, pTest_ForCheck_UnSafe.fValue);
+        }
+
+        [Test]
+        public void SaveLoad_EncryptTest()
+        {
+            SaveTest_Safe pTest = new SaveTest_Safe();
+            pTest.iValue = Random.Range(1, 100);
+            pTest.strValue = pTest.iValue.ToString();
+            pTest.fValue = Random.Range(-1f, 1f);
+
+            // 암호화 저장
+            PlayerPrefsExtension.SetObject_Encrypt(nameof(SaveLoad_EncryptTest), pTest);
+
+            SaveTest_Safe pTest_ForCheck = new SaveTest_Safe();
+
+            Assert.AreNotEqual(pTest.iValue, pTest_ForCheck.iValue);
+            Assert.AreNotEqual(pTest.strValue, pTest_ForCheck.strValue);
+            Assert.AreNotEqual(pTest.fValue, pTest_ForCheck.fValue);
+
+            // Encrypt한 오브젝트를 일반 Get으로 하면 에러
+            Assert.IsFalse(PlayerPrefsExtension.GetObject(nameof(SaveLoad_EncryptTest), ref pTest_ForCheck, false));
+            Assert.AreNotEqual(pTest.iValue, pTest_ForCheck.iValue);
+            Assert.AreNotEqual(pTest.strValue, pTest_ForCheck.strValue);
+            Assert.AreNotEqual(pTest.fValue, pTest_ForCheck.fValue);
+
+            // 암호화 불러오기
+            Assert.IsTrue(PlayerPrefsExtension.GetObject_Encrypted(nameof(SaveLoad_EncryptTest), ref pTest_ForCheck));
+            Assert.AreEqual(pTest.iValue, pTest_ForCheck.iValue);
+            Assert.AreEqual(pTest.strValue, pTest_ForCheck.strValue);
+            Assert.AreEqual(pTest.fValue, pTest_ForCheck.fValue);
+        }
+    }
+}
