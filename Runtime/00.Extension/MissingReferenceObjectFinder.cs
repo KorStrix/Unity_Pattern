2de253c9b4f3a69f50523dab4656566b6de32ae8
Strix <KorStrix@users.noreply.github.com>
2020-03-03 15:58:26 +0900

    no message

---
+++ Runtime/00.Extension/MissingReferenceObjectFinder.cs
@@ -0,0 +1,97 @@
+#region Header
+/*	============================================
+ *	Aurthor 			    : Strix
+ *	Initial Creation Date 	: 2020-03-03
+ *	Summary 		        : 
+ *  Template 		        : For Unity Editor V1
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// 
+/// </summary>
+// http://wiki.unity3d.com/index.php/FindMissingScripts
+public class MissingReferenceObjectFinder
+{
+    /* const & readonly declaration             */
+
+    /* enum & struct declaration                */
+
+    /* public - Field declaration               */
+
+    static int iGameObjectCount = 0, iComponentCount = 0, iMissing_count = 0;
+
+    [UnityEditor.MenuItem("GameObject/Find Missing Reference Component")]
+    public static void Find_Missing_Reference_Component()
+    {
+        GameObject[] arrObject = Selection.gameObjects;
+        foreach (GameObject pObject in arrObject)
+        {
+            FindInGO(pObject);
+        }
+
+        Debug.Log(string.Format("Searched {0} GameObjects, {1} components, found {2} missing", iGameObjectCount, iComponentCount, iMissing_count));
+    }
+
+    /* protected & private - Field declaration  */
+
+
+    // ========================================================================== //
+
+    /* public - [Do~Somthing] Function 	        */
+
+
+    // ========================================================================== //
+
+    /* protected - [Override & Unity API]       */
+
+
+    /* protected - [abstract & virtual]         */
+
+
+    // ========================================================================== //
+
+    #region Private
+    private static void FindInGO(GameObject pObject)
+    {
+        iGameObjectCount++;
+        Component[] arrComponent = pObject.GetComponents<Component>();
+        for (int i = 0; i < arrComponent.Length; i++)
+        {
+            iComponentCount++;
+            Component pComponent = arrComponent[i];
+            if (pComponent != null)
+                continue;
+
+            iMissing_count++;
+            string strObjectName = pObject.name;
+            Transform pTransform = pObject.transform;
+            while (pTransform.parent != null)
+            {
+                strObjectName = pTransform.parent.name + "/" + strObjectName;
+                pTransform = pTransform.parent;
+            }
+
+            Debug.Log(strObjectName + " has an empty script attached in position: " + i, pObject);
+            EditorGUIUtility.PingObject(pComponent);
+        }
+
+
+        // Now recurse through each child GO (if there are any):
+        foreach (Transform pChildObject in pObject.transform)
+        {
+            //Debug.Log("Searching " + childT.name  + " " );
+            FindInGO(pChildObject.gameObject);
+        }
+    }
+
+    #endregion Private
+}
